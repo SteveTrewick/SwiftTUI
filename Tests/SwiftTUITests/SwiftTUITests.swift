60df3b6c46decb4ef94445f3bf34271652da724b
Steve Trewick <SteveTrewick@users.noreply.github.com>
2025-09-27 18:07:00 +0100

    Fix circular buffer fullness tracking

--- Tests/SwiftTUITests/SwiftTUITests.swift
+++ Tests/SwiftTUITests/SwiftTUITests.swift
@@ -1,3 +1,58 @@
 import XCTest
 @testable import SwiftTUI
 
+final class CircularBufferTests: XCTestCase {
+
+    func testCircularBufferFullnessTransitions() {
+        var buffer = CircularBuffer<Int>(capacity: 3)
+
+        XCTAssertEqual(buffer.filled, 0)
+        XCTAssertFalse(buffer.isFull)
+
+        buffer.put(1)
+        XCTAssertEqual(buffer.filled, 1)
+        XCTAssertFalse(buffer.isFull)
+
+        buffer.put(2)
+        XCTAssertEqual(buffer.filled, 2)
+        XCTAssertFalse(buffer.isFull)
+
+        buffer.put(3)
+        XCTAssertEqual(buffer.filled, 3)
+        XCTAssertTrue(buffer.isFull)
+
+        _ = buffer.get()
+        XCTAssertEqual(buffer.filled, 2)
+        XCTAssertFalse(buffer.isFull)
+
+        buffer.put(4)
+        XCTAssertEqual(buffer.filled, 3)
+        XCTAssertTrue(buffer.isFull)
+
+        _ = buffer.get()
+        XCTAssertEqual(buffer.filled, 2)
+        XCTAssertFalse(buffer.isFull)
+
+        _ = buffer.get()
+        XCTAssertEqual(buffer.filled, 1)
+        XCTAssertFalse(buffer.isFull)
+
+        _ = buffer.get()
+        XCTAssertEqual(buffer.filled, 0)
+        XCTAssertFalse(buffer.isFull)
+
+        buffer.put(5)
+        buffer.put(6)
+        buffer.put(7)
+        XCTAssertTrue(buffer.isFull)
+
+        buffer.put(8)
+        XCTAssertTrue(buffer.isFull)
+        XCTAssertEqual(buffer.filled, 3)
+
+        _ = buffer.get()
+        XCTAssertEqual(buffer.filled, 2)
+        XCTAssertFalse(buffer.isFull)
+    }
+}
+
