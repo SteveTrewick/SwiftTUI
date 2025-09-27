5c2bb2e481733d1c52873b3ce12c72d3370a3c95
Steve Trewick <steve.trewick@icloud.com>
2025-09-27 17:44:51 +0100

    add serialport

--- Package.swift
+++ Package.swift
@@ -20,14 +20,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Trace", url: "https://github.com/SteveTrewick/Trace", from: "1.0.3"),
-        .package(name:"PosixInputStream", url: "https://github.com/SteveTrewick/PosixInputStream", from: "1.0.4")
+        .package(name:"PosixInputStream", url: "https://github.com/SteveTrewick/PosixInputStream", from: "1.0.4"),
+        .package(name: "SerialPort", url: "https://github.com/SteveTrewick/SerialPort", from: "1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftTUI",
-            dependencies: ["Trace", "PosixInputStream"]),
+            dependencies: ["Trace", "PosixInputStream", "SerialPort"]),
         .testTarget(
             name: "SwiftTUITests",
             dependencies: ["SwiftTUI"]),
