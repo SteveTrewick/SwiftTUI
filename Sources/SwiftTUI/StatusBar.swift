9e45a0fd82dae39562685b4925462e040f848a2a
Steve Trewick <steve.trewick@icloud.com>
2025-09-29 22:21:03 +0100

    rework statusbar

--- Sources/SwiftTUI/StatusBar.swift
+++ Sources/SwiftTUI/StatusBar.swift
@@ -1,37 +1,44 @@
 import Foundation
 
-public final class StatusBar {
+public final class StatusBar : Renderable {
 
   public var foreground: ANSIForecolor
   public var background: ANSIBackcolor
-
+  public var text      : String
+  
   private let output: OutputController
 
-  public init(
-    output: OutputController = OutputController(),
-    foreground: ANSIForecolor = .black,
-    background: ANSIBackcolor = .bgWhite
+  
+  public init (
+    text      : String,
+    foreground: ANSIForecolor    = .black,
+    background: ANSIBackcolor    = .bgWhite,
+    output    : OutputController = OutputController()
   ) {
-    self.output = output
+    self.text       = text
+    self.output     = output
     self.foreground = foreground
     self.background = background
   }
 
-  public func draw(text: String, in size: winsize) {
-    let row = Int(size.ws_row)
+  
+  public func render ( in size: winsize ) -> [AnsiSequence]? {
+    
+    let row     = Int(size.ws_row)
     let columns = Int(size.ws_col)
-    guard row > 0 && columns > 0 else { return }
+    
+    guard row > 0 && columns > 0 else { return nil }
 
-    let visibleText = String(text.prefix(columns))
+    let visibleText  = String(text.prefix(columns))
     let paddingCount = max(0, columns - visibleText.count)
-    let paddedText = visibleText + String(repeating: " ", count: paddingCount)
+    let paddedText   = visibleText + String(repeating: " ", count: paddingCount)
 
-    output.display(
-      .moveCursor(row: row, col: 1),
-      .backcolor(background),
-      .forecolor(foreground),
-      .text(paddedText),
+    return [
+      .moveCursor( row: row, col: 1 ),
+      .backcolor ( background ),
+      .forecolor ( foreground ),
+      .text      ( paddedText ),
       .resetcolor
-    )
+    ]
   }
 }
