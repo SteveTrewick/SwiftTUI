839d6434ca7b49c684bec3347e27f75693b610b6
Steve Trewick <SteveTrewick@users.noreply.github.com>
2025-09-29 23:17:31 +0100

    Guard ioctl request type for Linux

--- Sources/SwiftTUI/WindowChanges.swift
+++ Sources/SwiftTUI/WindowChanges.swift
@@ -5,44 +5,54 @@
 
 import Foundation
 
+#if os(Linux)
+private func ioctlWindowSize(_ winsizePointer: UnsafeMutablePointer<winsize>) -> Int32 {
+  ioctl(STDOUT_FILENO, UInt(TIOCGWINSZ), winsizePointer)
+}
+#else
+private func ioctlWindowSize(_ winsizePointer: UnsafeMutablePointer<winsize>) -> Int32 {
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, winsizePointer)
+}
+#endif
+
 public class WindowChanges {
-  
-  
+
+
   private let queue   : DispatchQueue
   private let source  : DispatchSourceSignal
-  
+
   public var size     = winsize()
   public var onChange : ( (winsize) -> Void )? = nil
-  
-  
+
+
   public init() {
-    
+
     self.queue  = DispatchQueue(label: "SIGWINCH")
     self.source = DispatchSource.makeSignalSource(signal: SIGWINCH, queue: queue)
-    
+
     // get initial window frame
-    if ioctl(STDOUT_FILENO, TIOCGWINSZ, &size) != 0 {
+    if ioctlWindowSize(&size) != 0 {
       log("IOCTL fail, could not dermine initial windows size")
     }
-    
+
     source.setEventHandler { [self] in
       var newsize = winsize()
-      if ioctl(STDOUT_FILENO, TIOCGWINSZ, &newsize) == 0 {
+      if ioctlWindowSize(&newsize) == 0 {
         size = newsize
         onChange?(newsize)
       }
     }
-  
+
   }
-  
-  
+
+
   public func track() {
     source.resume()
   }
-  
-  
+
+
   public func untrack() {
     source.cancel()
   }
-  
+
 }
