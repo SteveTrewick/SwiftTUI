0cbd2ed11ee7f5a72f3213c3a21b59475e46da26
Steve Trewick <steve.trewick@icloud.com>
2025-10-01 22:17:54 +0100

    update package dependencies to use later linux compatible versions

--- Package.swift
+++ Package.swift
@@ -19,9 +19,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Trace", url: "https://github.com/SteveTrewick/Trace", from: "1.0.3"),
-        .package(name:"PosixInputStream", url: "https://github.com/SteveTrewick/PosixInputStream", from: "1.0.4"),
-        .package(name: "SerialPort", url: "https://github.com/SteveTrewick/SerialPort", from: "1.0.5")
+        .package ( name: "Trace",            url: "https://github.com/SteveTrewick/Trace",            from: "1.0.3" ),
+        .package ( name: "PosixInputStream", url: "https://github.com/SteveTrewick/PosixInputStream", from: "1.0.5" ),
+        //.package ( name: "SerialPort",       url: "https://github.com/SteveTrewick/SerialPort",       from: "1.0.5" ),
+        .package ( name: "HexDump",          url: "https://github.com/SteveTrewick/HexDump",          from: "2.0.0" )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
